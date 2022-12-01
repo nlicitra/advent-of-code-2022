27295553ffc3896f62035d0945b9c968456424f9
nlicitra <nickerous@gmail.com>
2022-12-01 17:10:32 -0500

    initial commit

---
+++ src/day1.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+pub fn part1() -> usize {
+    return fs::read_to_string("./inputs/day1.txt")
+        .unwrap()
+        .split("\n\n")
+        .map(|s| {
+            s.split_whitespace()
+                .map(|n| n.parse::<usize>().unwrap())
+                .sum()
+        })
+        .max()
+        .unwrap();
+}
+
+pub fn part2() -> usize {
+    let mut calories = fs::read_to_string("./inputs/day1.txt")
+        .unwrap()
+        .split("\n\n")
+        .map(|s| {
+            s.split_whitespace()
+                .map(|n| n.parse::<usize>().unwrap())
+                .sum()
+        })
+        .collect::<Vec<usize>>();
+    calories.sort_by(|a, b| b.cmp(a));
+    return calories[0..3].iter().sum();
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+#![allow(unused)]
+mod day1;
+
+fn main() {
+    let result = day1::part2();
+    println!("{result}");
+}
